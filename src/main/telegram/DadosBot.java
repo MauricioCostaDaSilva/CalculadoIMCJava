12ff0fa5cb7e015bcd338add048aa6bfbeb1dea6
Mauriciocosvil <mauriciocostadasil@gmail.com>
2024-05-17 17:42:09 -0300

    first commit

---
+++ src/main/telegram/DadosBot.java
@@ -0,0 +1,6 @@
+package br.com.SimplIMC.bot.telegram;
+
+public class DadosBot {
+    public static final String BOT_TOKEN = "6914222572";
+    public static final String BOT_USER_NAME = "SimplIMC_bot";
+}
