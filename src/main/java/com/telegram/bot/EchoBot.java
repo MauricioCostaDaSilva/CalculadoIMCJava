af031c42ec4a063b8ad92eafcd66a1549108ada9
Bruno Carvalho de Araujo <brunocarvalhodearaujo@gmail.com>
2024-05-18 10:22:30 +0000

    ci(bin): adiciona subprojeto bin ci(compose.yml): remove configuração de imagem e ambiente do serviço telegram-bot refactor(EchoBot.java): corrige espaçamento no método consume refactor(Main.java): remove busca por variável de ambiente e define token diretamente

--- src/main/java/com/telegram/bot/EchoBot.java
+++ src/main/java/com/telegram/bot/EchoBot.java
@@ -15,7 +15,7 @@ public class EchoBot implements LongPollingSingleThreadUpdateConsumer {
   }
 
   @Override
-  public void consume(Update update) {
+  public void consume (Update update) {
     // We check if the update has a message and the message has text
     if (update.hasMessage() && update.getMessage().hasText()) {
       SendMessage message = null;
--- src/main/java/com/telegram/bot/Main.java
+++ src/main/java/com/telegram/bot/Main.java
@@ -4,7 +4,7 @@ import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
 public class Main {
   public static void main(String[] args) {
-    String botToken = System.getenv("TELEGRAM_BOT_TOKEN");
+    String botToken = "6914222572:AAFU9hseS6tCllA7LccU_klJ4bx02O6XfBA";
     // Using try-with-resources to allow auto close to run upon finishing
     try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
       botsApplication.registerBot(botToken, new EchoBot(botToken));
