c0c94df9ba1d4fae3e3587259eedb9920c1e054e
Mauriciocosvil <mauriciocostadasil@gmail.com>
2024-05-31 21:43:48 -0300

    Correção do texto e quebra de linha

--- src/main/java/com/telegram/bot/EchoBot.java
+++ src/main/java/com/telegram/bot/EchoBot.java
@@ -27,14 +27,8 @@ public class EchoBot implements LongPollingSingleThreadUpdateConsumer {
             .builder()
             .chatId(chatId)
             .text(
-                "Durma Bem: Garanta de 7 a 9 horas de sono por noite. A falta de sono pode afetar negativamente o metabolismo e aumentar o apetite.\n"
-                    +
-                    "Gerenciamento do Estresse:\n" +
-                    "\n" +
-                    "Reduza o Estresse: Pratique técnicas de relaxamento como meditação, ioga ou hobbies que goste. O estresse pode levar à alimentação emocional.\n"
-                    +
-                    "Acompanhamento Profissional:\n" +
-                    "\n" +
+                "Durma Bem: Garanta de 7 a 9 horas de sono por noite. A falta de sono pode afetar negativamente o metabolismo e aumentar o apetite.\n" + "\n" +
+"Reduza o Estresse: Pratique técnicas de relaxamento como meditação, ioga ou hobbies que goste. O estresse pode levar à alimentação emocional.\n" +"Acompanhamento Profissional:\n" +"\n" +
                     "Nutricionista: Um nutricionista pode ajudar a criar um plano alimentar personalizado e equilibrado.\n"
                     +
                     "Médico: Consulte um médico para avaliar sua saúde geral e descartar quaisquer condições médicas que possam estar afetando seu peso.\n"
@@ -47,9 +41,7 @@ public class EchoBot implements LongPollingSingleThreadUpdateConsumer {
                     +
                     "Educação e Suporte:\n" +
                     "\n" +
-                    "Aprenda sobre Nutrição e Fitness: Eduque-se sobre hábitos saudáveis e busque suporte, seja de grupos de apoio, amigos ou família.\n"
-                    +
-                    "Fazer mudanças graduais e sustentáveis é a chave para alcançar e manter um peso saudável. Lembre-se de que a jornada é pessoal e única, então encontre o que funciona melhor para você e seu estilo de vida.")
+                    "Aprenda sobre Nutrição e Fitness: Eduque-se sobre hábitos saudáveis e busque suporte, seja de grupos de apoio, amigos ou família.\n" + "Fazer mudanças graduais e sustentáveis é a chave para alcançar e manter um peso saudável. Lembre-se de que a jornada é pessoal e única, então encontre o que funciona melhor para você e seu estilo de vida.")
             .build();
       } else if (update.getMessage().getText().contains("/calcular")) {
         String[] parte = update.getMessage().getText().split(" ");
@@ -61,105 +53,25 @@ public class EchoBot implements LongPollingSingleThreadUpdateConsumer {
         String classificacao;
 
         if (imc < 18.5) {
-          classificacao = "Abaixo do peso \n Aumente a Ingestão Calórica: Consuma mais calorias do que você queima." + 
-                        "Isso significa aumentar o consumo de alimentos ricos em calorias, como" + 
-                        "gorduras saudáveis, carboidratos complexos e proteínas magras." + 
-                        "" + 
-                        "Faça Refeições Mais Frequentes: Em vez de apenas três grandes refeições por" + 
-                        "dia, faça várias refeições menores ao longo do dia para aumentar a ingestão de" + 
-                        "calorias. Isso também pode ajudar a evitar a sensação de estufamento e facilitar" + 
-                        "a ingestão de mais alimentos. " + 
-                        "Exercite-se com Foco em Ganho de Massa: Combine uma alimentação calórica com exercícios de resistência para ajudar a aumentar a massa muscular. Isso inclui levantamento de peso, exercícios de musculação e treinamento de força." + 
-                        "" + 
-                        "Evite Beber Água Antes das Refeições: Beber água antes das refeições pode reduzir o apetite. Se possível, tente beber líquidos após as refeições para evitar que a água encha o estômago." + 
-                        "" + 
-                        "";
+          classificacao = "Abaixo do peso: \n  → Aumente a Ingestão Calórica: \n" +  "Consuma mais calorias do que você queima. Isso significa aumentar o consumo de alimentos ricos em calorias, como gorduras saudáveis, carboidratos complexos e proteínas magras. \n → Exercite-se com Foco em Ganho de Massa: \n" +  "Combine uma alimentação calórica com exercícios de resistência para ajudar a aumentar a massa muscular.\n" + "→ Para melhores avaliações e orientação personalizada, consulte um profissional de saúde.";
         } else if (imc < 24.9)  {
-          classificacao = "Peso normal \n Manter peso" + 
-                        "Alimentação Balanceada: Priorize alimentos ricos em nutrientes, como frutas," + 
-                        "legumes, grãos integrais, proteínas magras e gorduras saudáveis. Evite" + 
-                        "alimentos processados, ricos em açúcares e gorduras saturadas." + 
-                        "" + 
-                        "Controle das Porções: Aprenda a reconhecer as porções adequadas para cada alimento." + 
-                        "" + 
-                        "Exercício Regular: O exercício ajuda a manter a massa muscular e a regular o metabolismo." + 
-                        "Monitore-se Regularmente: Pese-se regularmente e fique atento a sinais de ganho de peso. Isso pode ajudá-lo a identificar problemas precocemente e fazer ajustes na sua rotina, se necessário." + 
-                        "";
+          classificacao = "Peso normal: \n " + "→ Alimentação Balanceada: Priorize alimentos ricos em nutrientes, como frutas, legumes, grãos integrais, proteínas magras e gorduras saudáveis. \n" + "→ Exercício Regular: O exercício ajuda a manter a massa muscular e a regular o metabolismo.\n" + "→ Para melhores avaliações e orientação personalizada, consulte um profissional de saúde.";
         } else if (imc < 29.9) {
-          classificacao = "Sobrepeso \n Controle de Porções: Fique atento ao tamanho das porções e evite comer em excesso. Use pratos menores, preste atenção às suas sensações de fome e saciedade e pare de comer quando estiver satisfeito, não quando estiver completamente cheio." + 
-                        "Alimentação Equilibrada: Priorize uma alimentação balanceada, rica em frutas, vegetais, proteínas magras e grãos integrais." + 
-                        "Atividade Física Regular: Faça exercícios regularmente, incluindo atividades aeróbicas (como caminhada, corrida, natação, ciclismo) e exercícios de fortalecimento muscular." + 
-                        "Reduza o Sedentarismo: Além do exercício planejado, procure maneiras de ser mais ativo no dia a dia, como subir escadas em vez de usar o elevador, estacionar mais longe do destino para caminhar mais, e fazer pausas para se movimentar durante o trabalho." + 
-                        "";
+          classificacao = "Sobrepeso: \n "+ "→ Controle de Porções: Fique atento ao tamanho das porções e evite comer em excesso. Use pratos menores, preste atenção às suas sensações de fome e saciedade e pare de comer quando estiver satisfeito, não quando estiver completamente cheio.\n" + "→ Atividade Física Regular: Faça exercícios regularmente, incluindo atividades aeróbicas (como caminhada, corrida, natação, ciclismo) e exercícios de fortalecimento muscular.\n" + "→ Para melhores avaliações e orientação personalizada, consulte um profissional de saúde.";
         } else if (imc < 34.9) {
-          classificacao = "Obesidade grau 1 \n Alimentação saudável: Concentre-se em uma dieta rica em vegetais, frutas, grãos" + 
-                        "integrais e proteínas magras." + 
-                        "Controle de porções: A quantidade de comida que você consome é tão importante " + 
-                        "quanto a qualidade. Tente comer porções menores e preste atenção às suas sensações " + 
-                        "de fome e saciedade" + 
-                        "Exercício regular: Faça atividades físicas regularmente. Comece com algo que goste e " + 
-                        "aumente gradualmente a intensidade e a duração à medida que se sentir mais " + 
-                        "confortável" + 
-                        "Mantenha-se hidratado: Beba bastante água ao longo do dia. Às vezes, nosso corpo " + 
-                        "confunde a sede com fome, então beber água pode ajudar a reduzir o consumo " + 
-                        "excessivo de alimentos." + 
-                        "Durma o suficiente: A falta de sono pode afetar os hormônios relacionados à fome e " + 
-                        "ao apetite, levando a desejos por alimentos não saudáveis e excesso de ingestão." + 
-                        "Gerencie o estresse: O estresse crônico pode levar a hábitos alimentares pouco " + 
-                        "saudáveis, como comer emocionalmente" + 
-                        "Procure apoio: Ter apoio de amigos, familiares ou grupos de apoio pode ser " + 
-                        "fundamental para o sucesso a longo prazo na perda de peso e na manutenção de" + 
-                        "hábitos saudáveis";
+          classificacao = "Obesidade grau 1: \n " + "→ Controle de porções: A quantidade de comida que você consome é tão importante quanto a qualidade. Tente comer porções menores e preste atenção às suas sensações de fome e saciedade.\n" +"→ Reduza o Sedentarismo: Além de atividade Física Regular, procure maneiras de ser mais ativo no dia a dia, como subir escadas em vez de usar o elevador, estacionar mais longe do destino para caminhar mais, e fazer pausas para se movimentar durante o trabalho.\n" + "→ Para melhores avaliações e orientação personalizada, consulte um profissional de saúde.";
         } else if (imc < 39.9) {
-          classificacao = "Obesidade grau 2 \n Consulte um profissional de saúde: Antes de iniciar qualquer programa de perda de " + 
-                        "peso, é importante consultar um médico ou um nutricionista" + 
-                        "Estabeleça metas realistas: Defina metas de perda de peso alcançáveis e mensuráveis. " + 
-                        "Ao definir metas realistas, você será mais propenso a manter-se motivado e " + 
-                        "comprometido ao longo do tempo." + 
-                        "Acompanhe sua alimentação: Mantenha um diário alimentar para acompanhar o que " + 
-                        "você está comendo e beber." + 
-                        "Exercício regular e variado: Além de exercícios cardiovasculares, como caminhada, " + 
-                        "corrida ou natação, incorpore também exercícios de resistência, como musculação, para " + 
-                        "ajudar a aumentar sua massa muscular e queimar mais calorias" + 
-                        "Revise suas escolhas alimentares: Faça escolhas alimentares mais conscientes, " + 
-                        "optando por alimentos nutritivos e de baixa caloria" + 
-                        "Busque apoio social: Encontre o apoio de amigos, familiares ou grupos de apoio que " + 
-                        "possam incentivá-lo e motivá-lo ao longo do processo de perda de peso" + 
-                        "Cuide da sua saúde mental: A obesidade de grau 2 pode estar associada a problemas " + 
-                        "de autoestima e saúde mental" + 
-                        "Seja consistente e paciente: Seja paciente consigo mesmo e esteja preparado para " + 
-                        "enfrentar desafios ao longo do caminho.";
+          classificacao = "Obesidade grau 2: \n " +"→ Acompanhe sua alimentação: Mantenha um diário alimentar para acompanhar o que você está comendo e bebendo. Fique atento ao tamanho das porções e evite comer em excesso.\n" +"→ Exercício regular e variado: Além de exercícios cardiovasculares, como caminhada, corrida ou natação, incorpore também exercícios de resistência, como musculação, para ajudar a aumentar sua massa muscular e queimar mais calorias.\n" +"→ Para melhores avaliações e orientação personalizada, consulte um profissional de saúde.";
         } else {
-          classificacao = "Obesidade grau 3 \n Consulte um Profissional de Saúde: O primeiro passo é consultar um médico ou um " + 
-                        "nutricionista para avaliar sua situação de saúde e criar um plano personalizado para " + 
-                        "perda de peso." + 
-                        "Mudanças na Alimentação: Adote uma alimentação equilibrada e nutritiva, rica em " + 
-                        "frutas, vegetais, proteínas magras e grãos integrais" + 
-                        "Controle de Porções: Aprenda a controlar o tamanho das porções e evite comer em" + 
-                        "Exercício Regular: Faça atividade física regularmente, de preferência com a orientação " + 
-                        "de um profissional." + 
-                        "Estabeleça Metas Realistas: Defina metas de perda de peso realistas e alcançáveis a " + 
-                        "curto e longo prazo." + 
-                        "Mantenha-se Hidratado: Beba bastante água ao longo do dia, pois a " + 
-                        "desidratação pode ser confundida com fome." + 
-                        "Durma Suficientemente: Priorize um sono de qualidade, pois a falta de sono " + 
-                        "pode afetar os hormônios que regulam o apetite e o metabolismo." + 
-                        "Busque Apoio: Procure o apoio de amigos, familiares ou grupos de apoio para " + 
-                        "ajudá-lo durante sua jornada de perda de peso." + 
-                        "Evite Dietas Extremas: Evite dietas da moda ou extremamente restritivas, pois " + 
-                        "elas geralmente não são sustentáveis a longo prazo e podem causar danos à " + 
-                        "saúde." + 
-                        "Seja Paciente e Persistente: A perda de peso pode ser um processo desafiador " + 
-                        "e levar tempo. Mantenha-se focado em seus objetivos, seja paciente consigo " + 
-                        "mesmo e celebre cada conquista, por menor que seja.";
+          classificacao = "Obesidade grau 3: \n " + "→ Mudanças na Alimentação: Adote uma alimentação equilibrada e nutritiva, rica em frutas, vegetais, proteínas magras e grãos integrais.\n"+ "→ Exercício Regular: Faça atividade física regularmente, de preferência com a orientação de um profissional.\n" +"→ Para melhores avaliações e orientação personalizada, consulte um profissional de saúde.";
         }
         message = SendMessage
             .builder()
             .chatId(chatId)
             .text(
-              "Seu IMC é: " + imc + "\n" +
+              "→ Seu IMC É: " + imc + "\n" +
               "→ classificação: " + classificacao + "\n" +
-              "→ Quantidade de água para se manter saudavél:" + qAgua 
+              "→ Quantidade de água para se manter saudavél: " + qAgua + " ml." 
                )
             .build();
       } else if (message == null) {
